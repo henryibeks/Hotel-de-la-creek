a4daa90e951b094730de7fb8e95403af694f11e8
henrysouth <95618154+henrysouth@users.noreply.github.com>
2022-01-08 00:40:13 +0000

    Add files via upload
    A hotel website to explore booking options and a new generation of hotel webspace.

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let header= document.querySelector('.header')
+let hamburgerMenu= document.querySelector('.hamburger-menu')
+
+hamburgerMenu.addEventListener('click', function name(params) {
+    header.classList.toggle('menuopen');
+})
